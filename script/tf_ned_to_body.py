bcd7dcfa1560214c2294e0dcac2f7f34319800ee
S.O.N.I.A. Guest <sonia@ens.etsmtl.ca>
2016-11-14 16:06:47 -0500

    Working navigation
    Modification to the tf script to reduce the load on cpu
    * for tf_ned_to_body, we use spin without loop because in python,
      spin is basically a continous no-op (see rospy spin once on google)
    
    Removing unused transform in the launch file, since the twist part of odom message contains euler angles
    
    Modification rediect_imu_to_dvl:
    - Fixing the checksum calculation
    - Moving the conversion radian to degree to the imu callback
    - Removing the conversion code to use transforms3d package instead.
    - writting bytearray instead of string
    - changing loop to call .loop function to the IMUToDVL class.
    	* loop function is a blocking call, so it does not spin like crazy
    
    proc_navigation now takes less than 4 % CPU :)

--- script/tf_ned_to_body.py
+++ script/tf_ned_to_body.py
@@ -12,12 +12,19 @@ from nav_msgs.msg import Odometry
 class NedToBody:
     def __init__(self):
         self.br = tf.TransformBroadcaster()
-        self.sub_odom = rospy.Subscriber("/proc_navigation/odom", Odometry,
+        self.tf_send_divider = 10
+	self.tf_send_count = 0
+	self.sub_odom = rospy.Subscriber("/proc_navigation/odom", Odometry,
                                          self.odom_callback)
-        while not rospy.is_shutdown():
-            continue
-
+	
     def odom_callback(self, msg):
+	self.tf_send_count = self.tf_send_count + 1
+	# Enable dividing by divider to reduce load on CPU since it is 
+	# only for visualisation.
+	if( self.tf_send_divider != self.tf_send_count ):
+	    return
+	else:
+	    self.tf_send_count = 0
         p = (msg.pose.pose.position.x,
              msg.pose.pose.position.y,
              msg.pose.pose.position.z)
@@ -34,5 +41,6 @@ if __name__ == '__main__':
     rospy.init_node('tf_ned_to_body')
     try:
         ned_to_body = NedToBody()
+    	rospy.spin()
     except rospy.ROSInterruptException:
         pass
