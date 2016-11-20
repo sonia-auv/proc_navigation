64244c3c5a2d4b72081e9bfbe0181e3b4334089a
S.O.N.I.A. Guest <sonia@ens.etsmtl.ca>
2016-11-19 21:08:08 -0500

    Correcting navigation

--- src/proc_navigation/dvl_data.cc
+++ src/proc_navigation/dvl_data.cc
@@ -24,8 +24,8 @@ void DVLData::BottomTrackingCallback(sonia_msgs::BottomTracking msg) {
   if (!VerifyNAN(msg.velocity)) {
     // See p.195, search in page for: BT Velocity
     // Receive data as ENU
-    velocity_east = msg.velocity[0];
-    velocity_north = msg.velocity[1];
+    velocity_north = msg.velocity[0];
+    velocity_east = msg.velocity[1];
     velocity_up = msg.velocity[2];
     // For integration of speed.
     if (last_timestamp_us != -1) {
@@ -37,15 +37,14 @@ void DVLData::BottomTrackingCallback(sonia_msgs::BottomTracking msg) {
     }
 
     // speed is in mm/second
-
     // convert time to second
     double time_delta_s = (timestamp_us - last_timestamp_us) / 1000000.0f;
 
     // Convert velocities to meter on the fly
     // Convert from ENU to NED
-    position_xyz_m[0] += (velocity_north / 1000.0f) * time_delta_s;
-    position_xyz_m[1] += (velocity_east / 1000.0f) * time_delta_s;
-    position_xyz_m[2] += (-velocity_up / 1000.0f) * time_delta_s;
+    position_xyz_m[0] += (velocity_north) * time_delta_s;
+    position_xyz_m[1] += (velocity_east) * time_delta_s;
+    position_xyz_m[2] += (-velocity_up) * time_delta_s;
   } else {
     ROS_INFO_STREAM(
         " DVL - Bottom tracking - Received NAN on velocity.\n" << msg);
